d8545321f1ba01e9f0d3ed68537c93c95f8dc346
Fabrício Justo <fajusto@icloud.com>
2015-11-05 18:21:30 -0200

    Test index.php file initilized

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Test</title>
+	<style>
+	#formRow {
+		margin: 2px;
+	}
+	</style>
+</head>
+<body>
+<form name="CustomerForm">
+	<td>
+		<input id="formRow" type="text" name="firstName" placeholder="First Name" maxlength="50"/><br>
+		<input id="formRow" type="text" name="lastName" placeholder="Last Name" maxlength="50"/><br>
+		<input id="formRow" type="text" name="email" placeholder="E-mail" maxlength="70"/><br>
+		<input id="formRow" type="number" name="cpf" placeholder="CPF" maxlength="10"/><br>
+		<input id="formRow" type="number" name="birthday" placeholder="Birthday" maxlength="10"/><br>
+		<input name="Is_active" type="checkbox" value="1"/>Está ativo<br>
+	</td>
+</form>
+</body>
+</html>
