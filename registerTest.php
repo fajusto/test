3c4e5d6aa9432b176394e63b0741a82553154615
Fabrício Justo <fajusto@icloud.com>
2015-11-06 21:30:22 -0200

    registerTest.php edited

--- registerTest.php
+++ registerTest.php
@@ -1,9 +1,4 @@
-<html>
-<head>
-	<title>Registering...</title>
-</head>
-	<body>
-		<?php
+<?php
 		include_once('inc/conexao.php');
 			$firstName=$_POST['firstName'];
 			$lastName=$_POST['lastName'];
@@ -13,13 +8,10 @@
 			$Is_active=$_POST['Is_active'];
 			$sql = "INSERT INTO usuarios (firstName, lastName, email, cpf, birthday, Is_active)
 				VALUES('$firstName', '$lastName', '$email', '$cpf', '$birthday', '$Is_active')";
-			echo "<pre>";
-			var_dump($_POST);
-			$conn->close();
+
+			 	mysqli_query($conn, $sql);
+
 			$redirect = "http://localhost/test/index.php";
 			header("location:$redirect");
 		mysqli_close($conn);
-		?>
-	</body>
-</html>
-
+?>
