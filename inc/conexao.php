91b8704dbb0a5c90cf514bb1b45e99f4210acd2f
Fabrício Justo <fajusto@icloud.com>
2015-11-06 15:16:33 -0200

    database

---
+++ inc/conexao.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$banco = "cadastro";
+			$conexao = mysql_connect($servername, $username, $password) or die(mysql_error());
+			mysql_select_db($banco) or die(mysql_error());
--- registerTest.php
+++ registerTest.php
@@ -4,13 +4,6 @@
 </head>
 	<body>
 		<?php
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$banco = "cadastro";
-			$conexao = mysql_connect($servername, $username, $password) or die(mysql_error());
-			mysql_select_db($banco) or die(mysql_error());
-
 			$firstName=$_POST['firstName'];
 			$lastName=$_POST['lastName'];
 			$email=$_POST['email'];
