b6564b20656c510686e6e1115e02626c729fdba4
Fabrício Justo <fajusto@icloud.com>
2015-11-06 20:23:25 -0200

    store.php edited

--- edit.php
+++ edit.php
@@ -5,18 +5,22 @@
 </head>
 <body>
 <?php
-$sql =  "SELECT firstName, lastName, email, cpf, birthday, Is_active WHERE id=' . $_GET['id']  .  ' FROM usuarios";
+include_once('inc/conexao.php');
+$sql =  "SELECT id, firstName, lastName, email, cpf, birthday, Is_active  FROM usuarios WHERE id='" . $_GET['id']  .  "'";
 			$result = $conn->query($sql);
+			$usuario = $result->fetch_array();
+$conn->close();
 ?>
 
 <form name="CustomerForm" method="post" action="store.php">
-		<input id="formRow" type="text" name="firstName" placeholder="First Name" maxlength="50"/><br>
-		<input id="formRow" type="text" name="lastName" placeholder="Last Name" maxlength="50"/><br>
-		<input id="formRow" type="text" name="email" placeholder="E-mail" maxlength="70"/><br>
-		<input id="formRow" type="text" name="cpf" placeholder="CPF" maxlength="11"/><br>
-		<input id="formRow" type="date" name="birthday" placeholder="Birthday" maxlength="10"/><br>
-		<input name="Is_active" type="checkbox" value="1"/>Is active!<br>
+		<input id="formRow" type="text" name="firstName" placeholder="First Name" maxlength="50" value="<?= $usuario['firstName'] ?>"/><br>
+		<input id="formRow" type="text" name="lastName" placeholder="Last Name" maxlength="50" value="<?= $usuario['lastName'] ?>"/><br>
+		<input id="formRow" type="text" name="email" placeholder="E-mail" maxlength="70" value="<?= $usuario['email'] ?>"/><br>
+		<input id="formRow" type="text" name="cpf" placeholder="CPF" maxlength="11" value="<?= $usuario['cpf'] ?>"/><br>
+		<input id="formRow" type="date" name="birthday" placeholder="Birthday" maxlength="10" value="<?= $usuario['birthday'] ?>"/><br>
+		<input type="checkbox" value="1" <?= ($usuario['Is_active'] == 1) ? 'checked="checked"' : '' ?>>Is active!<br>
 		<input type="submit" value="Submit">
+		<input type="hidden" value="<?= $usuario['id'] ?>">
 </form>
 </body>
 </html>
--- store.php
+++ store.php
@@ -1,1 +1,13 @@
 <?php
+ 	include_once('inc/conexao.php');
+ 	$sql = "UPDATE usuarios SET firstName='" . $_POST['firstName'] . "', lastName='" . $_POST['lastName'] . "', email='" . $_POST['email'] . "', cpf='" . $_POST['cpf'] . "', birthday='" . $_POST['birthday'] . "', Is_active='" . $_POST['Is_active'] . "' WHERE id='" . $_POST['id'] . "'";
+
+var_dump($_POST); 
+ 	
+ 	if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
