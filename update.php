8d5a96193891eb6661743a80136509a4d5b3a7c2
Fabrício Justo <fajusto@icloud.com>
2015-11-09 16:38:57 -0200

    Update error 2

--- update.php
+++ update.php
@@ -1,12 +1,13 @@
 <?php
  		include_once('inc/conexao.php');
 	 	if(isset($_POST['Is_active'])){
-	 		$string = ", Is_active='" . $_POST['Is_active'] . "'";
+	 		// $string = ", Is_active='" . $_POST['Is_active'] . "'";
+	 		$sql = "UPDATE usuarios SET firstName='" . $_POST['firstName'] . "', lastName='" . $_POST['lastName'] . "', email='" . $_POST['email'] . "', cpf='" . $_POST['cpf'] . "', birthday='" . $_POST['birthday'] . "', Is_active='" . $_POST['Is_active'] . "' WHERE id='" . $_POST['id'] . "'";
 	 	} else {
-	 		$string = '';
+	 		$sql = "UPDATE usuarios SET firstName='" . $_POST['firstName'] . "', lastName='" . $_POST['lastName'] . "', email='" . $_POST['email'] . "', cpf='" . $_POST['cpf'] . "', birthday='" . $_POST['birthday'] . "' WHERE id='" . $_POST['id'] . "'";
 	 	}
- 		$sql = "UPDATE usuarios SET firstName='" . $_POST['firstName'] . "', lastName='" . $_POST['lastName'] . "', email='" . $_POST['email'] . "', cpf='" . $_POST['cpf'] . "', birthday='" . $_POST['birthday'] . "', " . $string . " WHERE id='" . $_POST['id'] . "'";
-var_dump($sql);
+ 		// $sql = "UPDATE usuarios SET firstName='" . $_POST['firstName'] . "', lastName='" . $_POST['lastName'] . "', email='" . $_POST['email'] . "', cpf='" . $_POST['cpf'] . "', birthday='" . $_POST['birthday'] . "', '" . $string . "' WHERE id='" . $_POST['id'] . "'";
+var_dump($_POST);
 	 	if (mysqli_query($conn, $sql)) {
 	     // header('Location: index.php');
 	 		echo 'Updated!';
